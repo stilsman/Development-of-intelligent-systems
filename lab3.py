9eb758be549f5268d437c064383b64f67c176240
stilsman <41634951+stilsman@users.noreply.github.com>
2024-12-25 16:15:03 +0600

    Update lab3.py without if

--- lab3.py
+++ lab3.py
@@ -1,136 +1,3 @@
-class Transition(object):
-    def __init__(self, toState):
-        self.toState = toState
-
-    def Execute(self):
-        pass
-
-
-class State(object):
-    def __init__(self, FSM):
-        self.FSM = FSM
-
-    def Execute(self):
-        pass
-
-class MovingUp(State):
-    def __init__(self, FSM):
-        super(MovingUp, self).__init__(FSM)
-
-    def Execute(self):
-        if self.FSM.char.current_floor < self.FSM.char.target_floor:
-            if self.FSM.char.current_floor == self.FSM.char.max_floors:
-                raise Exception(f"Elevator {self.FSM.char.elevator_id}: Cannot move up from the top floor")
-            else:
-                self.FSM.char.current_floor += 1
-                self.FSM.char.moves += 1
-                print(f"Elevator {self.FSM.char.elevator_id} moved up to floor {self.FSM.char.current_floor}")
-        if self.FSM.char.current_floor == self.FSM.char.target_floor:
-            self.FSM.ToTransition("toOpenDoor")
-
-class MovingDown(State):
-    def __init__(self, FSM):
-        super(MovingDown, self).__init__(FSM)
-
-    def Execute(self):
-        if self.FSM.char.current_floor > self.FSM.char.target_floor:
-            if self.FSM.char.current_floor == 1:
-                raise Exception(f"Elevator {self.FSM.char.elevator_id}: Cannot move down from the ground floor")
-            else:
-                self.FSM.char.current_floor -= 1
-                self.FSM.char.moves += 1
-                print(f"Elevator {self.FSM.char.elevator_id} moved down to floor {self.FSM.char.current_floor}")
-        if self.FSM.char.current_floor == self.FSM.char.target_floor:
-            self.FSM.ToTransition("toOpenDoor")
-
-class OpenDoor(State):
-    def __init__(self, FSM):
-        super(OpenDoor, self).__init__(FSM)
-
-    def Execute(self):
-        self.FSM.char.door_open = True
-        print(f"Elevator {self.FSM.char.elevator_id} doors are open")
-        self.FSM.ToTransition("toCloseDoor")
-
-
-
-class CloseDoor(State):
-    def __init__(self, FSM):
-        super(CloseDoor, self).__init__(FSM)
-
-    def Execute(self):
-        self.FSM.char.door_open = False
-        print(f"Elevator {self.FSM.char.elevator_id} doors are closed")
-        self.FSM.ToTransition("toHandler")
-
-class Handler(State):
-    def __init__(self, FSM):
-        super(Handler, self).__init__(FSM)
-
-    def Execute(self):
-        
-        if self.FSM.char.active_request:
-            call_floor, target_floor = self.FSM.char.active_request
-            
-            # лифт на этаже вызова
-            if self.FSM.char.current_floor == call_floor and self.FSM.char.target_floor == call_floor:
-                self.FSM.char.target_floor = target_floor
-                if self.FSM.char.current_floor < target_floor:
-                    self.FSM.ToTransition("toMovingUp")
-                elif self.FSM.char.current_floor > target_floor:
-                    self.FSM.ToTransition("toMovingDown")
-                else:
-                    self.FSM.ToTransition("toOpenDoor")
-
-            # лифт на целевом этаже
-            elif self.FSM.char.current_floor == self.FSM.char.target_floor:
-                self.FSM.char.active_request = None
-                self.FSM.char.target_floor = None
-                self.FSM.ToTransition("toHandler")
-
-            # лифт еще не на этаже вызова
-            else:
-                self.FSM.char.target_floor = call_floor
-                if self.FSM.char.current_floor < call_floor:
-                    self.FSM.ToTransition("toMovingUp")
-                elif self.FSM.char.current_floor > call_floor:
-                    self.FSM.ToTransition("toMovingDown")
-                else:
-                    self.FSM.ToTransition("toOpenDoor")
-        elif self.FSM.char.requests:
-            
-            self.FSM.char.active_request = self.FSM.char.requests.pop(0)
-
-
-
-class FSM(object):
-    def __init__(self, character):
-        self.char = character
-        self.states = {}
-        self.transitions = {}
-        self.curState = None
-        self.trans = None
-
-    def AddTransition(self, transName, transition):
-        self.transitions[transName] = transition
-
-    def AddState(self, stateName, state):
-        self.states[stateName] = state
-
-    def SetState(self, stateName):
-        self.curState = self.states[stateName]
-
-    def ToTransition(self, toTrans):
-        self.trans = self.transitions[toTrans]
-
-    def Execute(self):
-        if self.trans:
-            self.trans.Execute()
-            self.SetState(self.trans.toState)
-            self.trans = None
-        self.curState.Execute()
-
-
 class Elevator:
     def __init__(self, current_floor, floors, elevator_id):
         self.current_floor = current_floor
@@ -140,30 +7,52 @@ class Elevator:
         self.moves = 0
         self.max_floors = floors
         self.elevator_id = elevator_id
-        self.active_request = None
-        self.FSM = FSM(self)
 
-        self.FSM.AddState("Handler", Handler(self.FSM))
-        self.FSM.AddState("MovingUp", MovingUp(self.FSM))
-        self.FSM.AddState("MovingDown", MovingDown(self.FSM))
-        self.FSM.AddState("OpenDoor", OpenDoor(self.FSM))
-        self.FSM.AddState("CloseDoor", CloseDoor(self.FSM))
-
-        self.FSM.AddTransition("toHandler", Transition("Handler"))
-        self.FSM.AddTransition("toMovingUp", Transition("MovingUp"))
-        self.FSM.AddTransition("toMovingDown", Transition("MovingDown"))
-        self.FSM.AddTransition("toOpenDoor", Transition("OpenDoor"))
-        self.FSM.AddTransition("toCloseDoor", Transition("CloseDoor"))
-
-        self.FSM.SetState("Handler")
 
     def AddRequest(self, call_floor, target_floor):
         self.requests.append((call_floor, target_floor))
 
     def Execute(self):
-        self.FSM.Execute()
+        while self.requests:
+            calls = self.requests.pop(0)
+            for call in calls:
+                self.Move(call)
+                self.OpenDoor()
+                self.CloseDoor()
 
 
+    def Move(self, call):
+        while self.current_floor!= call:
+            {True: self.MovingUp, False: self.MovingDown}[self.current_floor < call]()
+
+    def MovingUp(self):
+        self.current_floor = self.checkAvailability(self.current_floor+1)
+        self.moves += 1
+        print(f"Elevator {self.elevator_id} moved up to floor {self.current_floor}")
+
+
+    def MovingDown(self):
+        self.current_floor = self.checkAvailability(self.current_floor-1)
+        self.moves += 1
+        print(f"Elevator {self.elevator_id} moved down to floor {self.current_floor}")
+
+    def OpenDoor(self):
+        self.door_open = True
+        print(f"Elevator {self.elevator_id} doors are open")
+
+    def CloseDoor(self):
+        self.door_open = False
+        print(f"Elevator {self.elevator_id} doors are closed")
+
+    def checkAvailability(self, floor):
+        move_limits = {
+            floor: floor for floor in range(1, self.max_floors+1)
+        }
+        return move_limits.get(floor) or self.FloorException(floor)
+    
+    def FloorException(self, floor):
+        raise Exception(f"Floor {floor} unavailable.")
+        
 
 def simulate_elevators(building_floors, elevators_initial, requests):
     elevators = [Elevator(current_floor, building_floors, i+1) for i, current_floor in enumerate(elevators_initial)]
@@ -176,10 +65,7 @@ def simulate_elevators(building_floors, elevators_initial, requests):
         )
 
         best_elevator.AddRequest(call_floor, target_floor)
-
-        while best_elevator.requests or best_elevator.active_request or best_elevator.FSM.curState != best_elevator.FSM.states["Handler"]:
-            best_elevator.Execute()
-
+        best_elevator.Execute()
         results.append(best_elevator.moves)
         best_elevator.moves = 0
         print()
